0594376cd486255a56e273cfdcfe3b97b9c35c9c
Paul Kinlan <paul.kinlan@gmail.com>
2016-12-21 14:20:26 +0000

    First stage of github request handler.

---
+++ cloud-functions/github-commit/index.js
@@ -0,0 +1,19 @@
+let gcloud = require('google-cloud');
+
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event The Cloud Functions event.
+ * @param {!Function} The callback function.
+ */
+exports.run = function subscribe(event, callback) {
+  // The Cloud Pub/Sub Message object.
+  const pubsubMessage = event.data;
+
+  // We're just going to log the message to prove that
+  // it worked.
+  console.log(Buffer.from(pubsubMessage.data, 'base64').toString());
+
+  // Don't forget to call the callback.
+  callback();
+};
---
+++ cloud-functions/github-commit/install.sh
@@ -0,0 +1,20 @@
+#! /usr/bin/bash
+
+# gcloud --project=web-push-rocks
+
+#gcloud components install alpha
+
+# Create the buckets
+gsutil mb -p web-push-rocks gs://web-push-rocks-staging-bucket
+
+# Create the topic that we send function calls to
+gcloud beta pubsub topics create send-github-commit
+
+# Create the cloud function [TEMPLATE]
+gcloud alpha functions deploy send- \
+  --stage-bucket gs://web-push-rocks-staging-bucket \
+  --trigger-provider cloud.pubsub \
+  --trigger-event topic.publish \
+  --trigger-resource send-github-commit \
+  --entry-point run \
+  --project web-push-rocks
