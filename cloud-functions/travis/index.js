eca2f50f9cffaf867b43bcf8f88e272373f4ea35
Paul Kinlan <paul.kinlan@gmail.com>
2016-12-21 19:10:40 +0000

    Adding a Travis cloud function.

---
+++ cloud-functions/travis/index.js
@@ -0,0 +1,38 @@
+let gcloud = require('google-cloud');
+const project_id = 'web-push-rocks';
+const pubsub = gcloud.pubsub({
+  projectId: project_id
+});
+
+const sendTopicId = `projects/${project_id}/topics/send`;
+
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event The Cloud Functions event.
+ * @param {!Function} The callback function.
+ */
+exports.run = function subscribe(event, callback) {
+  // The Cloud Pub/Sub Message object.
+  const pubsubMessage = event.data;
+  const message = pubsubMessage.message;
+
+  const sendTopic = pubsub.topic(sendTopicId);
+  const transformedMessage = {
+    "title": `Travis: ${message.status_message}`,
+    "description": `${message.status_message}`,
+    "url": `${message.build_url}`
+  }
+
+  sendTopic.publish({
+    id: id,
+    message: transformedMessage
+  });
+
+  // We're just going to log the message to prove that
+  // it worked.
+  console.log(Buffer.from(pubsubMessage.data, 'base64').toString());
+
+  // Don't forget to call the callback.
+  callback();
+};
---
+++ cloud-functions/travis/install.sh
@@ -0,0 +1,22 @@
+#! /usr/bin/bash
+
+# gcloud --project=web-push-rocks
+
+#gcloud components install alpha
+
+# Create the buckets
+gsutil mb -p web-push-rocks gs://web-push-rocks-staging-bucket
+gsutil mb -p web-push-rocks gs://web-push-rocks-source-bucket
+gsutil mb -p web-push-rocks gs://web-push-rocks-output-bucket
+
+# Create the topic that we send function calls to
+gcloud beta pubsub topics create send-travis
+
+# Create the cloud function [TEMPLATE]
+gcloud alpha functions deploy send-travis \
+  --stage-bucket gs://web-push-rocks-staging-bucket \
+  --trigger-provider cloud.pubsub \
+  --trigger-event topic.publish \
+  --trigger-resource send-travis \
+  --entry-point run \
+  --project web-push-rocks
