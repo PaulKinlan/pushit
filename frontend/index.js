d81d2db2e6bf0c7587e6d7cdd5e7b7f1bacaeea4
Paul Kinlan <paul.kinlan@gmail.com>
2016-12-21 20:38:41 +0000

    logging

--- frontend/index.js
+++ frontend/index.js
@@ -66,6 +66,8 @@ app.post('/send-raw', (req, res) => {
   const id = req.query.id;
   const processor = req.query.processor;
   
+  console.log('body request', req.body)
+
   // Topic must exist
   const rawTopic = pubsub.topic(`projects/${project_id}/topics/send-${processor}`);
   rawTopic.publish({
