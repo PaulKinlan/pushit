8367f81a879f4ca2ebb2611fc7955f306163e06b
Paul Kinlan <paul.kinlan@gmail.com>
2016-12-23 19:43:54 +0000

    Updating base template.

--- cloud-functions/raw/index.js
+++ cloud-functions/raw/index.js
@@ -1,4 +1,10 @@
 let gcloud = require('google-cloud');
+const project_id = 'web-push-rocks';
+const pubsub = gcloud.pubsub({
+  projectId: project_id
+});
+
+const sendTopicId = `projects/${project_id}/topics/send`;
 
 /**
  * Triggered from a message on a Cloud Pub/Sub topic.
@@ -9,6 +15,9 @@ let gcloud = require('google-cloud');
 exports.run = function subscribe(event, callback) {
   // The Cloud Pub/Sub Message object.
   const pubsubMessage = event.data;
+  const data = JSON.parse(Buffer.from(pubsubMessage.data, 'base64').toString());
+  const id = data.id;
+  const msgObject = JSON.parse(data.message.payload);
 
   // We're just going to log the message to prove that
   // it worked.
